261842d445e019d44819bc01b76946971006e2fc
Elí Maciel <tecno_eli@hotmail.com>
2025-09-23 10:53:31 -0600

    granular token

--- src/main/java/com/tokens/TokenType.java
+++ src/main/java/com/tokens/TokenType.java
@@ -1,5 +1,9 @@
 package com.tokens;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class TokenType {
     public static final String RESERVADA = "RESERVADA";
     public static final String TIPO = "TIPO";
@@ -9,4 +13,15 @@ public class TokenType {
     public static final String COMENTARIO = "COMENTARIO";
     public static final String SEPARADOR = "SEPARADOR";
     public static final String DESCONOCIDO = "DESCONOCIDO";
+
+    public static final Set<String> valores = new HashSet<>(Arrays.asList(
+        RESERVADA,
+        TIPO,
+        IDENTIFICADOR,
+        LITERAL,
+        OPERADOR,
+        COMENTARIO,
+        SEPARADOR,
+        DESCONOCIDO
+    ));
 }
