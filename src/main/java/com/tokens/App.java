30586b3a10de4391a5ad08c0ef37cc1c3c0b22cb
Elí Maciel <tecno_eli@hotmail.com>
2025-09-28 23:30:31 -0600

    log

--- src/main/java/com/tokens/App.java
+++ src/main/java/com/tokens/App.java
@@ -1,6 +1,7 @@
 package com.tokens;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
@@ -19,29 +20,17 @@ public class App {
             System.out.println("Tokens encontrados: " + tokens.size());
             System.out.println();
 
-            // for (var token : tokens) {
-            //     System.out.println("Tipo: "
-            //             + (token.getType().equals(TokenType.DESCONOCIDO) ? ConsoleColors.ANSI_RED + token.getType().name() + ConsoleColors.ANSI_RESET
-            //             : token.getType().equals(TokenType.LITERAL) ? ConsoleColors.ANSI_GREEN + token.getType().name() + ConsoleColors.ANSI_RESET
-            //             : token.getType().equals(TokenType.IDENTIFICADOR) ? ConsoleColors.ANSI_BLUE + token.getType().name() + ConsoleColors.ANSI_RESET
-            //             : token.getType().equals(TokenType.RESERVADA) ? ConsoleColors.ANSI_PURPLE + token.getType().name() + ConsoleColors.ANSI_RESET
-            //             : token.getType().name())
-            //             + " ".repeat(15 - token.getType().name().length()) + "Valor: "
-            //             + (token.getType() == TokenType.EOF ? "" : token.getValue())
-            //     );
-            // }
-
-            // // write tokens to file
-            // try (var log = new FileWriter("tokens.log")) {
-            //     for (var token : tokens) {
-            //         log.write("Tipo: "
-            //                 + token.getType().name()
-            //                 + " ".repeat(15 - token.getType().name().length()) + "Valor: "
-            //                 + token.getValue() + "\n");
-            //     }
-            // } catch (IOException e) {
-            //     System.out.println("No se pudo escribir el archivo.");
-            // }
+            // write tokens to file
+            try (var log = new FileWriter("tokens.log")) {
+                for (var token : tokens) {
+                    log.write("Tipo: "
+                            + token.getType().name()
+                            + " ".repeat(15 - token.getType().name().length()) + "Valor: "
+                            + token.getValue() + "\n");
+                }
+            } catch (IOException e) {
+                System.out.println("No se pudo escribir el archivo.");
+            }
 
             System.out.print(" Analizando expresión: ");
             for (var token : tokens) {
