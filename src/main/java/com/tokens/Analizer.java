5ff493863a3e59b475c758b3cf66f45919af251e
Elí Maciel <tecno_eli@hotmail.com>
2025-09-29 23:44:06 -0600

    BindingPower

--- src/main/java/com/tokens/Analizer.java
+++ src/main/java/com/tokens/Analizer.java
@@ -264,22 +264,3 @@ public class Analizer {
     }
 
 }
-
-class BindingPower {
-    private final float l;
-    private final float r;
-
-    public BindingPower(float left, float right) {
-        this.l = left;
-        this.r = right;
-    }
-
-    public float left() {
-        return l;
-    }
-
-    public float right() {
-        return r;
-    }
-
-}
