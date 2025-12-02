3d8076cc4da0dc4ff999f9c783b36c3f5bebd828
Elí Maciel <tecno_eli@hotmail.com>
2025-12-02 14:57:56 -0600

    create java file

--- src/main/java/com/compiler/Compiler.java
+++ src/main/java/com/compiler/Compiler.java
@@ -1,5 +1,7 @@
 package com.compiler;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.stream.Collectors;
 
 import com.compiler.ast.statements.BlockStatement;
@@ -7,16 +9,20 @@ import com.compiler.ast.statements.BlockStatement;
 public class Compiler {
     public static void compile(BlockStatement ast) {
         String gen = """
-public class App {
-    void println(Object obj) { IO.println(obj) }
-    public static void main(String[] args) {
-""";
+                public class App {
+                    void println(Object obj) { IO.println(obj); }
+                    public static void main(String[] args) {
+                """;
         gen += ast.getScript().lines().map(s -> "        " + s).collect(Collectors.joining("\n"));
         gen += """
 
-    }
-}
-""";
-        IO.println(gen);
+                    }
+                }
+                """;
+        try (var writer = new FileWriter("App.java")) {
+            writer.write(gen);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
