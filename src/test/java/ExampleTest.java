8746cbae7990b2951dd931b8cfc2dc5f11e3ba2e
Elí Maciel <tecno_eli@hotmail.com>
2025-12-08 01:46:44 -0600

    add more test

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,8 +1,10 @@
+
 import java.io.IOException;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.compiler.lexer.Lexer;
@@ -25,5 +27,54 @@ public class ExampleTest {
         var tokens = lexer.tokenize();
         var ast = Parser.parse(tokens);
         ast.validate();
+    }
+
+    @Test
+    void fallaSiTipoEsInvalido() throws IOException {
+        testFailingCase("invalid_type.java.txt");
+    }
+
+    @Test
+    void fallaSiHaySumaInvalida() throws IOException {
+        testFailingCase("invalid_add.java.txt");
+    }
+
+    @Test
+    void fallaSiVariableNoDeclarada() throws IOException {
+        testFailingCase("not_declared.java.txt");
+    }
+
+    @Test
+    void fallaSiVariableRedefinida() throws IOException {
+        testFailingCase("redeclare.java.txt");
+    }
+
+    @Test
+    void fallaSiFuncionInexistente() throws IOException {
+        testFailingCase("inexistent_function.java.txt");
+    }
+
+    @Test
+    void fallaSiParentesisNoCerrados() throws IOException {
+        testFailingCase("unclosed_parents.java.txt");
+    }
+
+    @Test
+    void fallaSiLiteralInvalido() throws IOException {
+        testFailingCase("literal_fail.java.txt");
+    }
+
+    @Test
+    void fallaSiRangoInvalido() throws IOException {
+        testFailingCase("range_error.java.txt");
+    }
+
+    void testFailingCase(String filename) throws IOException {
+        var source = Files.readString(Path.of("src/test/inputs/fails/", filename));
+        var lexer = new Lexer(source);
+        var tokens = lexer.tokenize();
+        Assertions.assertThrows(Exception.class, () -> {
+            Parser.parse(tokens).validate();
+        });
     }
 }
